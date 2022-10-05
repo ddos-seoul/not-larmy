f488fb007392ec83eb33ac6a6828962609ce4bf9
HwiSoo Kim <hwisoo@HwiSooui-MacBookAir.local>
2022-10-06 01:32:07 +0900

    error fix

--- resolvers/weather/summary_resolver.py
+++ resolvers/weather/summary_resolver.py
@@ -1,4 +1,5 @@
 import dataclasses
+from xmlrpc.client import Boolean
 
 from fastapi import APIRouter, HTTPException
 
@@ -9,17 +10,16 @@ router = APIRouter()
 
 @router.get("/")
 async def weather_summary(lat: float, lon: float):
+    if not(validate_args(lat,lon)):
+        raise HTTPException(status_code=400, detail="arguments 확인 필요")
     try:
-        if not (validate_args(lat, lon)):
-            raise HTTPException(status_code=400, detail="파라미터 값을-위도와 경도를- 확인해주세요.")
         summary_generator = GenerateSummaryUseCase(lat, lon)
-    except Exception as e:
-        raise HTTPException(status=500, detail="Internal Server Error")
-    summary_dict = dataclasses.asdict(await summary_generator.execute())
-    summary_dict["heads-up"] = summary_dict.pop("heads_up")
-    # TODO as_dict 혹은 __dict__ 수정
+        summary_dict = dataclasses.asdict(await summary_generator.execute())
+        summary_dict["heads-up"] = summary_dict.pop("heads_up")
+    except:
+        raise HTTPException(status_code=500, detail="서버 에러입니다.")
     return summary_dict
 
 
-def validate_args(lat, lon):
-    return lat >= -90 & lat <= 90 & lon > -180 & lon < 180
+def validate_args(lat: float, lon: float) -> Boolean:
+    return (lat >= -90.0) & (lat <= 90.0) & (lon > -180.0) & (lon < 180.0)
